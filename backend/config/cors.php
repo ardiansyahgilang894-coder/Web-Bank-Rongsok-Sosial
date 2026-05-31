f25ffa4bb0570cd5de0ec7039f1ac0ef6da3a611
Gilang Ardiansyah <takumachi35@gmail.com>
2026-05-31 16:53:43 +0700

    Fix CORS for Vercel frontend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://web-bank-rongsok-sosial-loopit.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
